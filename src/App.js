958d3e118620175cda0e8900aa26ffa57f8cb02c
Jazaltron <jazaltron10@gmail.com>
2021-06-25 21:05:44 +0100

    INside a Docker container

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World
+          Hello World Jasper is Here
         </p>
         <a
           className="App-link"
@@ -23,3 +23,30 @@ function App() {
 }
 
 export default App;
+
+
+
+// {
+//   "name":"Task Tracker",
+//   "dockerFile":"Dockerfile",
+//   "appPort": "[3000]",
+//   "runArgs": ["-u", "node"],
+//   "settings": {
+//       "workbench.colorTheme":"Andromeda Italic",
+//       /*"terminal.integrated.profiles.linux": {
+//           "zsh (login)": {
+//               "path": "zsh",
+//               "args": ["-l"]
+//           }
+//       }*/
+//   },
+//   "postCreateCommand": "npm install",
+//   "extensions": [
+//   "eliverlara.andromeda",
+//   "ms-azuretools.vscode-docker",
+//   "dsznajder.es7-react-js-snippets",
+//   "oderwat.indent-rainbow",
+//   "esbenp.prettier-vscode"
+// ]
+
+// }
