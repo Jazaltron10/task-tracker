608e16471260eb7d5d638664ef661d382db14b58
Jazaltron <jazaltron10@gmail.com>
2021-09-18 16:18:49 +0100

    Added the react-router-dom

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,11 +1,53 @@
+import {useState} from 'react'
+// Each input is going to have its own level of state, component level state not app level.
+const AddTask = ({onAdd}) => {
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
 
-const AddTask = () => {
+    const onSubmit = (e) => {
+        e.preventDefault()
+    
+        if(!text){
+            alert("Please add a task")
+            return
+        }
+
+        onAdd({text, day , reminder})
+        setText("")
+        setDay("")
+        setReminder(false)
+    }
+
+
     return (
-        <form className='add-form'>
+        <form className='add-form' onSubmit={onSubmit}>
             <div className='form-control'>
-                <label htmlFor=""></label>
-                <input type="text" placeholder='Add Task'/>
+                <label>Task</label>
+                <input type="text" 
+                placeholder='Add Task' 
+                value={text}
+                onChange={(e)=> setText(e.target.value)}
+                />
             </div>
+            <div className='form-control'>
+                <label>Day & Time</label>
+                <input type="text" 
+                placeholder='Add Day & Time'
+                value={day}
+                onChange={(e)=> setDay(e.target.value)}
+                />
+            </div>
+            <div className='form-control form-control-check'>
+                <label>Set Reminder</label>
+                <input type="checkbox"
+                value={reminder}
+                checked={reminder}
+                onChange={(e)=> setReminder(e.currentTarget.checked)}
+                />
+            </div>
+
+            <input type="submit" value="Save Task" className='btn btn-block' />
         </form>
     )
 }
